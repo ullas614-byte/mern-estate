2a51f6cdbe7ebc443afc0c290c6d637910c56f94
Ullas R G <ullas614@gmail.com>
2026-03-26 12:44:53 +0400

    complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,98 @@
-import React from 'react'
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function Signin() {
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(!res.ok){
+        setError(data.message || "Something went wrong");
+        setLoading(false);
+        return;
+      }else{
+        setMessage("Logging in...");
+        setLoading(false);
+        setTimeout(()=>{
+          navigate("/")
+        }, 2000)
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
-    <div>Signin</div>
-  )
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="bg-white p-8 rounded-xl shadow-lg w-96"
+      >
+        <h2 className="text-2xl font-bold mb-4 text-center">Signin</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full p-2 mb-3 border rounded"
+            id="email"
+            onChange={handleChange}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full p-2 mb-4 border rounded"
+            id="password"
+            onChange={handleChange}
+          />
+
+          <motion.button
+            disabled={loading}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className="w-full bg-blue-800 text-white p-2 rounded"
+          >
+            {loading ? "Loading..." : "Sign In"}
+          </motion.button>
+          {message && (
+  <p className="text-green-600 text-center mt-3">{message}</p>
+)}
+
+{error && (
+  <p className="text-red-600 text-center mt-3">{error}</p>
+)}
+        </form>
+        <div className="flex gap-2">
+          <p>Dont have an account?</p>
+          <Link to={"/sign-up"}>
+            <span className="text-blue-700">Sign up</span>
+          </Link>
+        </div>
+      </motion.div>
+    </div>
+  );
 }
