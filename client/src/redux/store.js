e3064366b8ab29afe04c46912278b002ae81e612
Ullas R G <ullas614@gmail.com>
2026-03-26 15:41:52 +0400

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,49 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice.js'
+// store.js
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';
+
+// ✅ custom safe storage for browser
+const storage = {
+  getItem: (key) => {
+    try {
+      return Promise.resolve(window.localStorage.getItem(key));
+    } catch {
+      return Promise.resolve(null);
+    }
+  },
+  setItem: (key, value) => {
+    try {
+      window.localStorage.setItem(key, value);
+      return Promise.resolve(value);
+    } catch {
+      return Promise.resolve(value);
+    }
+  },
+  removeItem: (key) => {
+    try {
+      window.localStorage.removeItem(key);
+      return Promise.resolve();
+    } catch {
+      return Promise.resolve();
+    }
+  },
+};
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user: userReducer},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false,                                    ////not get error when get serializing any variable(prevent any error in browser)
-  }),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
